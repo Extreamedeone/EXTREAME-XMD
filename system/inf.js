6536b946aee576cfbdadd01941c91bd7cfeb1183
Extreamedeone <kiokonzau921@gmail.com>
2025-08-06 22:53:26 +0300

    Update inf.js

--- system/inf.js
+++ system/inf.js
@@ -1,18 +1,4 @@
-/*const channelInfo = {
-  'contextInfo': {
-    'forwardingScore': 0x3e7,
-    'isForwarded': true,
-    'forwardedNewsletterMessageInfo': {
-      'newsletterJid': '120363419072079836@newsletter',
-      'newsletterName': "EXTREAME-XMD ",
-      'serverMessageId': -1
-    }
-  }
-};
-module.exports = {
-  'channelInfo': channelInfo
-};
-*/
+
 
 
 
