8766ea27e1920678afae99cd68711f797b2c2b89
Extreamedeone <kiokonzau921@gmail.com>
2025-08-09 14:40:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && pkg update && pkg upgrade -y && pkg install nodejs git python ffmpeg unzip libwebp libjpeg-turbo libpng imagemagick libcaca clang libvpx -y
+apt update && apt upgrade -y && pkg update && pkg upgrade -y && pkg install nodejs git python ffmpeg unzip libwebp libjpeg-turbo libpng imagemagick libcaca yarn clang libvpx -y
 git clone https://github.com/Extreamedeone/EXTREAME-XMD
 cd EXTREAME-XMD
 npm install
