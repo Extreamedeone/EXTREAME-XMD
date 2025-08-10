38d2eec7b99d60d9b4390059577e9f2541d1e13d
Extreamedeone <kiokonzau921@gmail.com>
2025-08-10 14:44:44 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps:[{
     name: "EXTREAME-XMD",
     script: "index.js",
-    watch: true,
+    watch: false,
+    autorestart: true,
     env: {
-      NODE_ENV: "production",
-      SESSION_ID: "Your_session_id _here"
+      NODE_ENV: "production"
     }
   }]
 }
